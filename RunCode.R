5a623756c0f0b58ef91f6c38b8331c56ea3e3f4a
Kara Belknap <kbelkna@ncsu.edu>
2022-11-01 13:08:15 -0400

    updating with all channels.

--- RunCode.R
+++ RunCode.R
@@ -21,4 +21,12 @@
 
 rmarkdown::render("Belknap_Monti_project3_ST558.Rmd", output_file = "lifestyle.md", params = list(channel = "lifestyle"))
 
+rmarkdown::render("Belknap_Monti_project3_ST558.Rmd", output_file = "entertainment.md", params = list(channel = "entertainment"))
+
 rmarkdown::render("Belknap_Monti_project3_ST558.Rmd", output_file = "bus.md", params = list(channel = "bus"))
+
+rmarkdown::render("Belknap_Monti_project3_ST558.Rmd", output_file = "socmed.md", params = list(channel = "socmed"))
+
+rmarkdown::render("Belknap_Monti_project3_ST558.Rmd", output_file = "tech.md", params = list(channel = "tech"))
+
+rmarkdown::render("Belknap_Monti_project3_ST558.Rmd", output_file = "world.md", params = list(channel = "world"))
