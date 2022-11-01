f29c71c075e8ea903dbdd72d07d212d378eb31f2
kbelkna <112021695+kbelkna@users.noreply.github.com>
2022-11-01 12:48:44 -0400

    Update RunCode.R
    testing rmarkdown/render for multiple params

--- RunCode.R
+++ RunCode.R
@@ -1,21 +1,24 @@
 
-rmarkdown::render(input = "Belknap_Monti_project3_ST558.Rmd",
-                  output_file = "README.md",
-                  output_format = "github_document",
-                  output_options = list(
-                    name_value_pairs = "value",
-                    or_something = TRUE,
-                    html_preview = FALSE,
-                    toc = TRUE,
-                    df_print = "tibble"
-                  ),
-                  runtime = "static",
-                  clean = TRUE,
-                  params = NULL,
-                  knit_meta = NULL,
-                  envir = parent.frame(),
-                  run_pandoc = TRUE,
-                  quiet = FALSE,
-                  encoding = "UTF-8"
-)
+# rmarkdown::render(input = "Belknap_Monti_project3_ST558.Rmd",
+#                   output_file = "README.md",
+#                   output_format = "github_document",
+#                   output_options = list(
+#                     name_value_pairs = "value",
+#                     or_something = TRUE,
+#                     html_preview = FALSE,
+#                     toc = TRUE,
+#                     df_print = "tibble"
+#                   ),
+#                   runtime = "static",
+#                   clean = TRUE,
+#                   params = NULL,
+#                   knit_meta = NULL,
+#                   envir = parent.frame(),
+#                   run_pandoc = TRUE,
+#                   quiet = FALSE,
+#                   encoding = "UTF-8"
+# )
 
+rmarkdown::render("Belknap_Monti_project3_ST558.Rmd", output_file = "lifestyle.html", params = list(channel = "lifestyle"))
+
+rmarkdown::render("Belknap_Monti_project3_ST558.Rmd", output_file = "bus.html", params = list(channel = "bus"))
